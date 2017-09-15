ca4d94e2b7e10837b722c9142ec94771b45a0924
David Rolston <gizmola@gmail.com>
2017-09-15 09:09:21 -0700

    Spread & Collect

--- app/index.js
+++ app/index.js
@@ -1,3 +1,15 @@
-let a = `good`;
-let greeting = `${a} morning`;
-console.log(greeting);
+let a = [20, 30, 40];
+let b = [10, ...a, 50];
+console.log(b);
+{
+  let a = ['Bob', 'Carol', 'Frank'];
+  let b = ['Mark', 'Sam', 'William', ...a];
+  console.log(b);
+}
+
+function collect(...a) {
+  console.log(a);
+}
+
+collect(1, 2, 3, 4, 5);
+collect(3, 6, 9, 77);
