49393dbf0036ac50a2a0af2570b9b047ff634333
David Rolston <gizmola@gmail.com>
2017-09-15 12:18:35 -0700

    Filter example

--- app/index.js
+++ app/index.js
@@ -1,20 +1,3 @@
-let values = [20, 30, 40];
-
-let double = (n) => {
-  return n*2;
-}
-
-// Using map
-let doubled = values.map(double);
-console.log(doubled);
-
-// Equivalent to
-let doubled2 = values.map((n) => {
-  return n*2;
-});
-console.log(doubled2);
-
-//Equivalent to
-// Note that return is assumed now
-let doubled3 = values.map((n) => n*2);
-console.log(doubled3);
+let points = [7, 16, 21, 4, 3, 22, 5];
+let highScores = points.filter((n) => n > 15);
+console.log(highScores);
