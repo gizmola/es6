07ce863a290e819bfcf06879206cbde96a48f49c
David Rolston <gizmola@gmail.com>
2017-09-26 16:59:11 -0700

    Map - Exercise

--- app/index.js
+++ app/index.js
@@ -1,18 +1,19 @@
-let numArr = [[1, 'one'],[2, 'two'], [3, 'three']];
-let valMap = new Map(numArr);
-// Iterate through Map
-for (let [key, value] of valMap.entries()) {
-  console.log(`${key} points to ${value}`);
-}
+let string = 'supercalifragilisticexpialidocious';
 
-let string = 'aqewrjqalkjflajvnvcfdlkjaewrwrefljav';
-let letters = new Map();
-for (let i=0; i<string.length; i++) {
-  let letter = string[i];
-  if (!letters.has(letter)) {
-    letters.set(letter, 1);
-  } else {
-    letters.set(letter, letters.get(letter) + 1);
+const countLetter = (word, orig_letter) => {
+  // TODO Create a map called 'letters'
+  let letters = new Map();
+  for (let i=0; i<word.length; i++) {
+    let letter = word[i];
+    if (!letters.has(letter)) {
+      letters.set(letter, 1);
+    } else {
+      letters.set(letter, letters.get(letter) + 1);
+    }
   }
-}
-console.log(letters);
+  return letters.get(orig_letter);
+};
+
+let a_count = countLetter(string, 'a');
+let x_count = countLetter(string, 'x');
+console.log(a_count, x_count);
