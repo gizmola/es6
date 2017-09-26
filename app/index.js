e664846e3b539c09dab3c6569ab224c76a3ff698
David Rolston <gizmola@gmail.com>
2017-09-26 16:40:40 -0700

    Maps - basics

--- app/index.js
+++ app/index.js
@@ -1,26 +1,8 @@
-let a = new Set();
-
-a.add(5);
-a.add(43);
-a.add("Greetings");
-a.add({x: 50, y: 200});
-
+let a = new Map();
+let key_1 = "string key";
+let key_2 = { a: 'key' };
+let key_3 = function() {};
+a.set(key_1, 'return value for a string key');
+a.set(key_2, 'return value for an object key');
+a.set(key_3, 'return value for a function key');
 console.log(a);
-console.log(a.size);
-console.log(a.has(5));
-console.log(a.has(7));
-
-let numbers = [5, 7, 8, 13, 17];
-let numSet = new Set(numbers);
-console.log(numSet);
-
-//enhanced for loop
-for (let element of numSet.values()) {
-  console.log(element);
-}
-
-let chars = 'alkfawerlfuruownslje';
-let chars_arr = chars.split("");
-let chars_set = new Set(chars_arr);
-//Set get only uni
-console.log(chars_set);
