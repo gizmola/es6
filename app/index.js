7484ff343264b30e703c27c96c1ebb794ee89599
David Rolston <gizmola@gmail.com>
2017-09-26 12:23:22 -0700

    Classes & Inheritance basics

--- app/index.js
+++ app/index.js
@@ -9,6 +9,17 @@ class Animal {
   }
 }
 
-let king = new Animal("Mufasa", 4.5);
-console.log(king);
-king.hello();
+class Lion extends Animal {
+  constructor(name, height, color) {
+    super(name, height);
+    this.color = color;
+  }
+
+  hello() {
+    console.log(`Hi! I'm ${this.name} from Pride Rock!`);
+  }
+}
+
+let son = new Lion("Simba", 2, "golden");
+console.log(son);
+son.hello();
