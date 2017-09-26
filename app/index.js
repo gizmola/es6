ee4b097a3243f8360940d7b7da1be5e9db7563a7
David Rolston <gizmola@gmail.com>
2017-09-26 12:52:00 -0700

    Prototypes

--- app/index.js
+++ app/index.js
@@ -1,8 +1,22 @@
-class Calculator {
-  static multiply(a, b) {
-    return a*b;
-  }
+function Wizard(name, house, pet) {
+  this.name = name;
+  this.house = house;
+  this.pet = pet;
+
+  this.greet = () => `I'm ${this.name} from ${this.house}`;
+
 }
 
-let a = Calculator.multiply(5,7);
-console.log(a);
+Wizard.prototype.pet_name;
+// Won't work: no enclosing object to provide this context
+// Wizard.prototype.info = () => return `I have a ${this.pet} named ${this.pet_name}`;
+
+// Need an actual function
+Wizard.prototype.info = function() {
+  return `I have a ${this.pet} named ${this.pet_name}`;
+}
+
+let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
+harry.pet_name = "Hedwig";
+console.log(harry);
+console.log(harry.greet());
