6fcd1de18e9736c1efede53b3bab053989d5e35c
David Rolston <gizmola@gmail.com>
2017-09-27 10:41:19 -0700

    Closure factory exercise - minimal arrow function, syntax fixed

--- app/index.js
+++ app/index.js
@@ -1,33 +1,6 @@
-// Function factories
-const addSuffix = (x) => {
-  const concat = (y) => {
-    return y + x;
-  }
-  return concat;
-}
+const addFactory = x => y => y + x;
 
-let add_ness = addSuffix("ness");
-console.log(add_ness);
-let h = add_ness("happi");
-console.log(h);
-
-let add_ful = addSuffix("ful");
-let f = add_ful("fruit");
-console.log(f);
-
-const product = (x) => {
-  return y => {
-    return y * x;
-  }
-}
-
-// This can be shortened to:
-const product2 = (x) => y => y * x;
-
-let mult5 = product(5);
-let double = product(2);
-let double2 = product2(2);
-
-console.log(mult5(3));
-console.log(double(9));
-console.log(double2(9));
+const add50 = addFactory(50);
+console.log(add50(100));
+const add30 = addFactory(30);
+console.log(add30(75));
