ec2b2c03ee047ac3895b0052ad14744ad31544cb
David Rolston <gizmola@gmail.com>
2017-09-26 17:04:51 -0700

    Closure - example 1

--- app/index.js
+++ app/index.js
@@ -1,19 +1,11 @@
-let string = 'supercalifragilisticexpialidocious';
-
-const countLetter = (word, orig_letter) => {
-  // TODO Create a map called 'letters'
-  let letters = new Map();
-  for (let i=0; i<word.length; i++) {
-    let letter = word[i];
-    if (!letters.has(letter)) {
-      letters.set(letter, 1);
-    } else {
-      letters.set(letter, letters.get(letter) + 1);
-    }
+let call = () => {
+  let secret = 'ES6 rocks1';
+  let reveal = () => {
+    console.log(secret);
   }
-  return letters.get(orig_letter);
-};
-
-let a_count = countLetter(string, 'a');
-let x_count = countLetter(string, 'x');
-console.log(a_count, x_count);
+  reveal();
+}
+// Can't access secret
+//console.log(secret)
+// But you can still execute it to get at the data in the closure
+call();
