229e3980e99cf87325f48eb8e395448886466cd7
David Rolston <gizmola@gmail.com>
2017-09-28 20:15:05 -0700

    Generator - Simple setup

--- app/index.js
+++ app/index.js
@@ -1,25 +1,7 @@
 function* letterMaker() {
-  yield 'a';
-  yield 'b';
-  yield 'c';
+  yield 'x';
+  yield 'y';
+  yield 'z';
 }
 
 let letterGen = letterMaker();
-console.log(letterGen.next().value);
-console.log(letterGen.next().value);
-console.log(letterGen.next().value);
-console.log(letterGen.next().value);
-
-function* countMaker() {
-  let count = 0;
-  while (count < 4) {
-    yield count += 1;
-  }
-}
-
-let countGen = countMaker();
-
-console.log(countGen.next().value);
-console.log(countGen.next().value);
-console.log(countGen.next().value);
-console.log(countGen.next().value);
