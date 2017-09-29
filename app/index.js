31554984eceee92cb7f0c02526331ab558e5e608
David Rolston <gizmola@gmail.com>
2017-09-28 20:52:03 -0700

    Promises - Async example

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 let p = new Promise((resolve, reject) => {
-  //resolve('Resolved promise data');
-  reject('Rejected promise data');
+  setTimeout(() => resolve('Resolved promise data'), 5000);
+  //reject('Rejected promise data');
 })
 p.then(response => console.log(response))
 .catch(error => console.log(error));
+
+console.log('after promise consumption');
