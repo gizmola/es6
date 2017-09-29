3524157ac0d7e040cb61ba66cdaff71f08b3b652
David Rolston <gizmola@gmail.com>
2017-09-28 20:34:12 -0700

    Generator Iterator - Simplified argument yield

--- app/index.js
+++ app/index.js
@@ -1,30 +1,17 @@
-const arrayIterator = (array) => {
-  let index = 0;
-
-  return {
-    next: () => {
-      if (index < array.length) {
-        let next = array[index];
-        index += 1;
-        return next;
-      }
-    }
-  }
-}
-
-let it = arrayIterator([1,2,3]);
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-
-
 // Generator Iterator
+/* Original
 function*  arrayIterator2() {
+  // Enhanced for loop
   for (let arg of arguments) {
     yield arg;
   }
 }
+*/
+// Simplified
+function*  arrayIterator2() {
+  yield* arguments;
+}
+
 
 var it2 = arrayIterator2(1, 2, 3);
 console.log(it2.next().value);
