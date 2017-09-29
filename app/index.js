0ce0ee0e1edd045047c187f8a9334d83eb152c3a
David Rolston <gizmola@gmail.com>
2017-09-28 20:48:38 -0700

    Promises - resolve, reject

--- app/index.js
+++ app/index.js
@@ -1,19 +1,6 @@
-// Generator Iterator
-/* Original
-function*  arrayIterator2() {
-  // Enhanced for loop
-  for (let arg of arguments) {
-    yield arg;
-  }
-}
-*/
-// Simplified
-function*  arrayIterator2() {
-  yield* arguments;
-}
-
-
-var it2 = arrayIterator2(1, 2, 3);
-console.log(it2.next().value);
-console.log(it2.next().value);
-console.log(it2.next().value);
+let p = new Promise((resolve, reject) => {
+  //resolve('Resolved promise data');
+  reject('Rejected promise data');
+})
+p.then(response => console.log(response))
+.catch(error => console.log(error));
