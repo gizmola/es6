a190f7deca865f45b13e0a9d3e1640fc09ce6270
David Rolston <gizmola@gmail.com>
2017-09-28 21:11:22 -0700

    fetch - example 2

--- app/index.js
+++ app/index.js
@@ -1,6 +1,4 @@
-// Free json REST server
-// https://jsonplaceholder.typicode.com/
-const root = 'https://jsonplaceholder.typicode.com/posts/1';
+const root = 'https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699';
 
 fetch(root, { method: "GET"})
   .then(response => response.json())
